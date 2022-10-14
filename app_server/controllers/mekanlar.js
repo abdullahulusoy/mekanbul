ed3263fea64ca5112e8373f708b441a407a250a5
ulusoy32 <abdullah303132@gmail.com>
2022-10-14 14:58:35 +0300

    ikinci ders

---
+++ app_server/controllers/mekanlar.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+const anaSayfa = function (req, res, next) {
+  res.render('anaSayfa', { title: 'anaSayfa' });
+}
+
+const mekanBilgisi = function (req, res, next) {
+  res.render('mekanBilgisi', { title: 'mekanBilgisi' });
+}
+
+const yorumEkle = function (req, res, next) {
+  res.render('yorumEkle', { title: 'yorumEkle' });
+}
+
+module.exports = {
+
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+
+
+}
