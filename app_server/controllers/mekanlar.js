4af2cb720cf1f9b856b500751514f4d99cf876c8
ulusoy32 <abdullah303132@gmail.com>
2022-12-01 20:47:09 +0300

    guncelleme

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,91 +1,109 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const anaSayfa = function (req, res) {
-    res.render('anasayfa', {
-        'baslik': 'Ana Sayfa',
-        "sayfaBaslik":
-        {
-            "siteAd": "Mekanbul",
-            "slogan": "Civardaki Mekanları Keşfet!"
-        },
-        "mekanlar": [
-            {
-                "ad": "Gloria jeans",
-                "adres": "Iyaş AVM ",
-                "puan": "4",
-                "mesafe": "2km",
-                "imkanlar": ["Kahve", "Tatlı"]
-            },
-            {
-                "ad": "Adıyaman Çiğköfte",
-                "adres": "Bahçelievler Mahallesi",
-                "puan": "5",
-                "mesafe": "1km",
-                "imkanlar": ["Dürüm", "Doritoslu"]
-            }
-        ]
+const axios = require("axios")
+var apiSecenekleri={
+  sunucu : "http://localhost:3000",
+  apiYolu : "/api/mekanlar/"
+}
 
+var mesafeyiFormatla=function(mesafe){
+  var yeniMesafe, birim;
+  if(mesafe>1){
+    yeniMesafe=parseFloat(mesafe).toFixed(1);
+    birim=" km";
+  }else{
+    yeniMesafe=parseInt(mesafe*1000,10);
+    birim=" m";
+  }
+  return yeniMesafe+birim;
+}
+
+var anaSayfaOlustur=function(res,mekanListesi){
+  var mesaj;
+  if(!(mekanListesi instanceof Array)){
+    mesaj:"API HATASI: birşeyler ters gitti.";
+    mekanListesi=[];
+  }else{
+    if(!(mekanListesi.lenght)){
+      mesaj:"Civarda herhangi bir mekan yok";
     }
-
-
-    );
+  }
+  res.render("anasayfa",{
+    "baslik":"Anasayfa",
+    sayfaBaslik:{
+      "siteAd":"Mekanbul",
+      "slogan":"Mekanları Keşfet"
+    },
+    mekanlar:mekanListesi,
+    mesaj:mesaj
+  });
 }
 
-const mekanBilgisi = function (req, res) {
-    res.render('mekanBilgisi',
+/* GET home page. */
+const anaSayfa = function (req, res, next) {
+  axios.get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu, {
+    params:{
+      enlem:req.query.enlem,
+      boylam:req.query.boylam
+    }
+  }).then(function(response){
+    var i, mekanlar;
+    mekanlar = response.data;
+    for(i=0; i<mekanlar.length; i++){
+      mekanlar[i].mesafe = mesafeyiFormatla(mekanlar[i].mesafe);
+    }
+    anaSayfaOlustur(res, mekanlar);
+  }).catch(function(hata){
+    anaSayfaOlustur(res,hata);
+  });
+}
+
+const mekanBilgisi = function (req, res, next) {
+  res.render("mekanbilgisi", {
+    'baslik' : 'Mekan Bilgisi',
+    'mekanBaslik' : 'Starbucks',
+    'mekanDetay' : {
+      'ad' : 'Starbucks',
+      'puan' : 3,
+      'adres' : 'SurYapı Park Avm',
+      'saatler' : [
         {
-            'baslik': 'Mekan bilgisi',
-            "mekanBaslik": "Gloria jeans",
-            "mekanDetay": {
-                "ad": "Gloria jeans",
-                "adres": "Iyaş AVM",
-                "puan": "4",
-                "saatler": [
-                    {
-                        "gunler": "Pazartesi-Cuma",
-                        "acilis": "10:00",
-                        "kapanis": "02:00",
-                        "kapali": "false"
-                    },
-                    {
-                        "gunler": "Cumartesi-Pazar",
-                        "acilis": "10:00",
-                        "kapanis": "02:00",
-                        "kapali": "false"
-                    }
-                ],
-                "imkanlar": ["Kahve", "Tatlı"],
-                "koordinatlar": {
-                    "enlem": "37.78",
-                    "boylam": "30.54"
-                },
-                "yorumlar":[
-                    {
-                        "yorumYapan":"Abdullah Mert Ulusoy",
-                        "puan":"4",
-                        "tarih":"13 Ekim 2022",
-                        "yorumMetni":"Kahveler güzel."
-                    },
-                    {
-                        "yorumYapan":"Murat Ulusoy",
-                        "puan":"2",
-                        "tarih":"20 Ekim 2022",
-                        "yorumMetni":"Kahveler berbat."
-                    }
+          'gunler' : 'Pazartesi-Cuma',
+          'acilis' : '9:00',
+          'kapanis' : "23:00",
+          'kapali' : false
+        },
+        {
+          'gunler' : 'Cumartesi-Pazar',
+          'acilis' : '8:00',
+          'kapanis' : "24:00",
+          'kapali' : false
+        },
+      ],
+      'imkanlar' : ["Coffee",'Tea','Cake'],
+      'koordinatlar' : 
+      {
+        'enlem' : '37.7',
+        'boylam' : '35.4'
+      },
+      'yorumlar' : [
+        {
+          'yorumYapan' : 'Hakan Körhasan',
+          'yorumMetni' : 'Offff Çok İYİİİİİ!!!',
+          'tarih' : '26 Ekim 2022',
+          'puan' : 5
+        }
+      ]
+    }
+  });
+};
 
-                ]
-
-            }
-
-        });
-}
-
-const yorumEkle = function (req, res) {
-    res.render('yorumEkle', { 'title': 'Yorum Sayfası' });
-}
+const yorumEkle = function (req, res, next) {
+  res.render("yorumekle", { title: "Yorum Ekle" });
+};
 
 module.exports = {
-    anaSayfa,
-    mekanBilgisi,
-    yorumEkle
-}
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle
+};
